9254ac3684778e73c80b407b7fd7d1955d829ae5
Raquel Moss <raquelxmoss@gmail.com>
2017-02-18 10:11:03 +1300

    Prettify website

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,33 @@
-console.log('This would be the main JS file.');
+$(document).ready(function () {
+  smoothScroll();
+  mobileNav();
+});
+
+function smoothScroll () {
+  $('.menu a').click(function (e) {
+    e.preventDefault();
+
+    var anchor = $(e.target).attr('href');
+
+    $('html, body').animate({
+      scrollTop: $(anchor).offset().top - 20
+    }, 300);
+  });
+}
+
+function mobileNav () {
+  var $sidebar = $('aside.sidebar');
+
+  $('.mobile-menu').click(function () {
+    $sidebar.addClass('active');
+    $sidebar.show(500);
+  });
+
+  $('.inner-container').click(function (event) {
+    if (!$sidebar.hasClass('active')) { return; }
+    if ($(event.target).is('.menu') || $(event.target).parent().is('.menu')) { return; }
+
+    $('aside.sidebar').hide(500);
+    $('aside.sidebar').removeClass('active');
+  });
+}
